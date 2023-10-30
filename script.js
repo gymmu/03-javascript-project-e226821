0bec1523157963c6c94e1c54f13f0f8a0ebab545
Ann-Cathrine Böller <e226821@edu.sbl.ch>
2023-10-30 11:41:06 +0100

    Aufgabe 1 und 2

--- script.js
+++ script.js
@@ -4,7 +4,33 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+// Ist das Zeichen ein e, so hänge nichts weiter an.
+if (currentElement === 'e') {
+  result.push("") 
+  // Ist das Zeichen ein E, so hänge nichts weiter an. 
+} else if  ( currentElement === 'E') {
+    result.push("")
+  }else { //Das Zeichen soll weder ein e noch ein E sein.
+  result.push(currentElement) 
+}
+  }
+  return result.join ("")
+}
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+const currentElement = input[i]
+
+// Wandle das aktuelle Element in einen Grossbuchstaben um
+const capitalizedElement = currentElement.toUpperCase()
+
+//Hänge den Grossbuchstaben an das Resultat an..
+result.push(capitalizedElement)
+
   }
   return result.join("")
 }
