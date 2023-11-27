f71bdd050f69ce925d1ce11caaafb74e980ddcf2
Ann-Cathrine Böller <e226821@edu.sbl.ch>
2023-11-27 12:08:01 +0100

    Aufgabe 19

--- script.js
+++ script.js
@@ -348,6 +348,20 @@ for (let i = 0; i < input.length; i++) {
 
 }
 
+export function aufgabe19 (args) {
+  const input = args
+  const result = []
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    //zu dem Element wird das gleiche Element nochmals angesetzt.
+    result.push(currentElement + currentElement )
+  }
+ 
+  return result.join("")
+}
+
+
 
 
 
